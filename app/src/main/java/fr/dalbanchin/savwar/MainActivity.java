e4c0452062962d19e551023147284f786406b4d7
Théo <theo.dalban@gmail.com>
2020-10-09 15:44:11 +0200

    Importation vue savoir du jour ( manque le responsive sur certaines parties )

--- app/src/main/java/fr/dalbanchin/savwar/MainActivity.java
+++ app/src/main/java/fr/dalbanchin/savwar/MainActivity.java
@@ -1,13 +1,36 @@
 package fr.dalbanchin.savwar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Bitmap;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    @SuppressLint("NewApi")
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            if (data != null) {
+                Bundle bundle = data.getExtras();
+                if (bundle != null) {
+                    ImageView image = findViewById(R.id.logo);
+                    image.setImageBitmap((Bitmap) bundle.get("data"));
+                    image.setImageTintList(new ColorStateList(new int[0][0], new int[0])); // Enlève le tint
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/fr/dalbanchin/savwar/savoir_du_jour_activity.java
@@ -0,0 +1,13 @@
+package fr.dalbanchin.savwar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class savoir_du_jour_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_savoir_du_jour_activity);
+    }
+}

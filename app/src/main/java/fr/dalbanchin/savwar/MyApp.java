fa4d1c989f0ab50331a67611173a97ba93dd5f75
Thomas Chin <thomas07.chin600@gmail.com>
2020-11-11 15:49:56 +0100

    Ajout page de chargement avant le lancement de l'appli.

---
+++ app/src/main/java/fr/dalbanchin/savwar/MyApp.java
@@ -0,0 +1,13 @@
+package fr.dalbanchin.savwar;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemClock.sleep(2000);
+    }
+}

e4872a97034da48f6b3a12dc84d6f0d7e58a574a
Thomas Chin <thomas07.chin600@gmail.com>
2020-11-12 23:14:13 +0100

    Animation et son dans splashScreen (chargement).

--- app/src/main/java/fr/dalbanchin/savwar/activities/SplashActivity.java
+++ app/src/main/java/fr/dalbanchin/savwar/activities/SplashActivity.java
@@ -1,19 +1,44 @@
 package fr.dalbanchin.savwar.activities;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
-import fr.dalbanchin.savwar.activities.MainActivity;
+import java.io.IOException;
+
+import fr.dalbanchin.savwar.R;
 
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView logo;
+    private static int splashTimeOut=3500;
+    MediaPlayer mySong;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_splash);
+        logo=(ImageView)findViewById(R.id.logo);
+        mySong = MediaPlayer.create(this, R.raw.splash);
+        mySong.setVolume(0.1f,0.1f);
+
+        mySong.start();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },splashTimeOut);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mysplashanimation);
+        logo.startAnimation(myanim);
     }
 }
